2e82263dc0765ed03f4fc814c6c1384438ac1f0a
Jessie Elkins <43121364+jessicaelkins@users.noreply.github.com>
2018-11-15 20:54:31 -0600

    Draft 4

--- src/MenuWindow.java
+++ src/MenuWindow.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 
@@ -41,9 +44,11 @@ public class MenuWindow {
 		
 		// create menu frame
 		frameMenu = new JFrame();
-		frameMenu.setBounds(100, 100, 1280, 780);
+		frameMenu.setTitle("Grandma's Cookbook");
+		frameMenu.setBounds(100, 100, 1280, 687);
 		frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameMenu.getContentPane().setLayout(null);
+		
 		
 		//create header panel
 		JPanel headerPanel = new JPanel();
@@ -60,43 +65,78 @@ public class MenuWindow {
 		
 		// create menu panel
 		JPanel menuPanel = new JPanel();
-		menuPanel.setBounds(71, 178, 489, 469);
+		menuPanel.setBounds(71, 162, 489, 469);
 		frameMenu.getContentPane().add(menuPanel);
 		menuPanel.setLayout(null);
 		
 		// create add recipe button
 		JButton btnAddRecipe = new JButton("ADD RECIPE");
+		btnAddRecipe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				AddRecipeWindow.newRecipe();
+				frameMenu.dispose();
+			}
+		});
 		btnAddRecipe.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnAddRecipe.setBounds(53, 32, 408, 61);
 		menuPanel.add(btnAddRecipe);
 		
 		// create breakfast button
 		JButton btnBreakfast = new JButton("BREAKFAST");
+		btnBreakfast.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				BreakfastRecipes.BreakfastScreen();
+				frameMenu.dispose();
+			}
+		});
 		btnBreakfast.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnBreakfast.setBounds(53, 132, 408, 61);
 		menuPanel.add(btnBreakfast);
 		
 		// create lunch button
 		JButton btnLunch = new JButton("LUNCH");
+		btnLunch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				LunchRecipes.LunchScreen();
+				frameMenu.dispose();
+			}
+		});
 		btnLunch.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnLunch.setBounds(53, 219, 408, 61);
 		menuPanel.add(btnLunch);
 		
 		// create dinner button
 		JButton btnDinner = new JButton("DINNER");
+		btnDinner.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				DinnerRecipes.DinnerScreen();
+				frameMenu.dispose();
+			}
+		});
 		btnDinner.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnDinner.setBounds(53, 300, 408, 61);
 		menuPanel.add(btnDinner);
 		
 		// create dessert button
 		JButton btnDessert = new JButton("DESSERT");
+		btnDessert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				DessertRecipes.DessertScreen();
+				frameMenu.dispose();
+			}
+		});
 		btnDessert.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnDessert.setBounds(53, 383, 408, 61);
 		menuPanel.add(btnDessert);
 		
 		// create picture panel
-		JPanel panel = new JPanel();
-		panel.setBounds(609, 178, 626, 469);
-		frameMenu.getContentPane().add(panel);
+		JPanel panelPicture = new JPanel();
+		panelPicture.setBounds(609, 162, 626, 469);
+		frameMenu.getContentPane().add(panelPicture);
 	}
 }
