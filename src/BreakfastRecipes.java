8f12ec7ef95e2dc998402f6f188294e4229539a1
Jessie Elkins <43121364+jessicaelkins@users.noreply.github.com>
2018-11-15 20:55:56 -0600

    Draft 4

--- src/BreakfastRecipes.java
+++ src/BreakfastRecipes.java
@@ -1,11 +1,13 @@
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Font;
 import javax.swing.JButton;
-import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BreakfastRecipes {
 
@@ -14,7 +16,7 @@ public class BreakfastRecipes {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void BreakfastScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -41,6 +43,7 @@ public class BreakfastRecipes {
 		
 		// create breakfast menu frame
 		frameBreakfast = new JFrame();
+		frameBreakfast.setTitle("Breakfast Recipe");
 		frameBreakfast.setBounds(100, 100, 1280, 780);
 		frameBreakfast.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameBreakfast.getContentPane().setLayout(null);
@@ -57,10 +60,30 @@ public class BreakfastRecipes {
 		lblBreakfastRecipes.setBounds(261, 20, 487, 73);
 		panelBreakfast.add(lblBreakfastRecipes);
 		
+		
 		// create breakfast result panel
 		JPanel panelBreakfastResults = new JPanel();
 		panelBreakfastResults.setBounds(141, 215, 986, 472);
 		frameBreakfast.getContentPane().add(panelBreakfastResults);
 		panelBreakfastResults.setLayout(null);
+		
+		// create back button panel
+		JPanel panelBack = new JPanel();
+		panelBack.setBounds(10, 45, 115, 50);
+		frameBreakfast.getContentPane().add(panelBack);
+		panelBack.setLayout(null);
+		
+		// create back button
+		JButton btnBack = new JButton("BACK");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				MenuWindow.main(null);
+				frameBreakfast.dispose();
+			}
+		});
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBack.setBounds(10, 10, 95, 30);
+		panelBack.add(btnBack);
 	}
 }
