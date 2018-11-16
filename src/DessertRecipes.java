4ea73eabdd5c484615ec844e27e91bdbb07caaa1
Jessie Elkins <43121364+jessicaelkins@users.noreply.github.com>
2018-11-15 20:59:13 -0600

    Draft 4

--- src/DessertRecipes.java
+++ src/DessertRecipes.java
@@ -4,6 +4,9 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DessertRecipes {
 
@@ -12,7 +15,7 @@ public class DessertRecipes {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void DessertScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -39,9 +42,11 @@ private void initialize() {
 		
 		// create dessert menu frame
 		frameDessert = new JFrame();
-		frameDessert.setBounds(100, 100, 1280, 780);
+		frameDessert.setTitle("Dessert Recipes");
+		frameDessert.setBounds(100, 100, 1280, 735);
 		frameDessert.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameDessert.getContentPane().setLayout(null);
+		
 		
 		// create dessert panel
 		JPanel panelDessert = new JPanel();
@@ -57,9 +62,26 @@ private void initialize() {
 		
 		// create dessert results panel
 		JPanel panelDessertResults = new JPanel();
-		panelDessertResults.setBounds(141, 215, 986, 472);
+		panelDessertResults.setBounds(141, 199, 986, 472);
 		frameDessert.getContentPane().add(panelDessertResults);
 		panelDessertResults.setLayout(null);
+		
+		// create back button panel
+		JPanel panelBack = new JPanel();
+		panelBack.setBounds(10, 45, 121, 62);
+		frameDessert.getContentPane().add(panelBack);
+		panelBack.setLayout(null);
+		
+		// create back button
+		JButton btnBack = new JButton("BACK");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuWindow.main(null);
+				frameDessert.dispose();
+			}
+		});
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBack.setBounds(10, 10, 101, 42);
+		panelBack.add(btnBack);
 	}
 }
-
