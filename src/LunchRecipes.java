c8076d3660280ea1b1e0c74fb2e6cde4409cdb25
Jessie Elkins <43121364+jessicaelkins@users.noreply.github.com>
2018-11-15 20:56:56 -0600

    Draft 4

--- src/LunchRecipes.java
+++ src/LunchRecipes.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class LunchRecipes {
@@ -13,7 +16,7 @@ public class LunchRecipes {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void LunchScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -40,9 +43,11 @@ public class LunchRecipes {
 		
 		// create lunch menu frame
 		frameLunch = new JFrame();
-		frameLunch.setBounds(100, 100, 1280, 780);
+		frameLunch.setTitle("Lunch Recipes");
+		frameLunch.setBounds(100, 100, 1280, 746);
 		frameLunch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameLunch.getContentPane().setLayout(null);
+		
 		
 		// create lunch panel
 		JPanel panelLunch = new JPanel();
@@ -61,5 +66,24 @@ public class LunchRecipes {
 		panelLunchResults.setBounds(141, 215, 986, 472);
 		frameLunch.getContentPane().add(panelLunchResults);
 		panelLunchResults.setLayout(null);
+		
+		// create back button panel
+		JPanel panelBack = new JPanel();
+		panelBack.setBounds(10, 45, 122, 59);
+		frameLunch.getContentPane().add(panelBack);
+		panelBack.setLayout(null);
+		
+		// create back button
+		JButton btnBack = new JButton("BACK");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				MenuWindow.main(null);
+				frameLunch.dispose();
+			}
+		});
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBack.setBounds(10, 10, 102, 39);
+		panelBack.add(btnBack);
 	}
 }
